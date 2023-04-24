7424f91e305fa20bf738d7e1cbfb450e8ab872d1
itrealms <it@rcrealms.org>
2023-04-24 12:50:12 -0700

    new files

---
+++ Projects/Py Runner/py2bat.py
@@ -0,0 +1,21 @@
+# Code here is always executed
+from rich.traceback import install
+
+install()
+
+if __name__ == '__main__':
+	# Code here Executed when invoked directly (Not a module)
+	import glob
+	import re
+
+	file_list = [file for file in glob.glob(f'*')]
+	for file_path in file_list:
+		if re.search(r'\.py', file_path):
+			bat_file = re.sub(r'\.py', '.bat', file_path)
+			with open(bat_file, 'w') as new_bat:
+				new_bat.write(f'@echo off\n')
+				new_bat.write(f'py "{file_path}"')
+
+else:
+	# Code here executed when imported (As a module))
+	pass
